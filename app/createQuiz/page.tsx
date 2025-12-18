e85a61e4c91948b0fe2e786ad5af8ddfc6810e1c
Kshitij Sharma <sharmakshitij250@gmail.com>
2025-12-18 17:47:24 +0530

    feat: fixed question number margin

--- app/createQuiz/page.tsx
+++ app/createQuiz/page.tsx
@@ -130,7 +130,7 @@ const page = () => {
                 }}
                 layout
                 key={ques.id}
-                className="grid grid-cols-[3%_90%_10%] p-10 "
+                className="grid md:grid-cols-[3%_90%_10%] grid-cols-[8%_90%_10%] p-10 "
               >
                 <H2Title text={`${(ind + 1).toString()}.`} />
                 <div>
